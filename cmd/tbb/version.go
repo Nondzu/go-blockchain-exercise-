801e6e382213dec4c8703461eba4bcfae481383e
Nondzu <kamil.nonckiewicz@gmail.com>
2023-01-12 16:46:27 +0100

    update versionm

--- cmd/tbb/version.go
+++ cmd/tbb/version.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
 const Major = "0"
 const Minor = "1"
-const Fix = "1"
+const Fix = "2"
 const Verbal = "TX Add && Balances List"
 
 var versionCmd = &cobra.Command{
